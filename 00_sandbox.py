f73bc5b0fe215ec59c5bc38854ccf0e107e48567
ayerst9198 <78511779+ayerst9198@users.noreply.github.com>
2023-05-16 10:47:50 +1200

    Add files via upload

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,57 +1,57 @@
-import tkinter as tk
+import ctypes
 import time
 
-root = tk.Tk()
-root.title("Dancing Banana")
-root.geometry("400x450")
+# Constants for key codes and window actions
+VK_LWIN = 0x5B
+VK_M = 0x4D
+SW_MINIMIZE = 6
 
-canvas = tk.Canvas(root, width=400, height=450)
-canvas.pack()
+# Define the keybd_event() function from the Win32 API
+SendInput = ctypes.windll.user32.SendInput
+PUL = ctypes.POINTER(ctypes.c_ulong)
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time",ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                ("mi", MouseInput),
+                ("hi", HardwareInput)]
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+def keybd_event(key, flags):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput(key, key, flags, 0, ctypes.pointer(extra))
+    x = Input(ctypes.c_ulong(1), ii_)
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
-# add a red water-like background
-bg = canvas.create_rectangle(0, 0, 400, 450, fill="red")
+# Define a function to minimize the active window
+def minimize_window():
+    keybd_event(VK_LWIN, 0)  # Press the left Windows key
+    keybd_event(VK_M, 0)     # Press the "M" key
+    time.sleep(0.1)          # Wait for the window to minimize
+    keybd_event(VK_M, 2)     # Release the "M" key
+    keybd_event(VK_LWIN, 2)  # Release the left Windows key
 
-body = canvas.create_oval(150, 100, 250, 300, fill="yellow")
-stem = canvas.create_rectangle(195, 75, 205, 100, fill="brown")
-left_arm = canvas.create_rectangle(100, 170, 150, 180, fill="yellow")
-right_arm = canvas.create_rectangle(250, 170, 300, 180, fill="yellow")
-left_leg = canvas.create_rectangle(175, 290, 185, 340, fill="brown")
-right_leg = canvas.create_rectangle(215, 290, 225, 340, fill="brown")
-face = canvas.create_oval(175, 125, 225, 175, fill="white")
-left_eye = canvas.create_oval(190, 145, 195, 150, fill="black")
-right_eye = canvas.create_oval(210, 145, 215, 150, fill="black")
-mouth = canvas.create_arc(180, 155, 220, 170, start=180, extent=180, fill="red")
-
-message = canvas.create_text(200, 350, text="Flying Banana God", font=("Helvetica", 20))
-
+# Loop until the "p" key is pressed
 while True:
-    for i in range(10):
-        canvas.move(body, 0, -5)
-        canvas.move(stem, 0, -5)
-        canvas.move(left_arm, 0, -5)
-        canvas.move(right_arm, 0, -5)
-        canvas.move(left_leg, 0, -5)
-        canvas.move(right_leg, 0, -5)
-        canvas.move(face, 0, -5)
-        canvas.move(left_eye, 0, -5)
-        canvas.move(right_eye, 0, -5)
-        canvas.move(mouth, 0, -5)
-        canvas.move(message, 0, -5)
-        root.update()
-        time.sleep(0.05)
-    for i in range(10):
-        canvas.move(body, 0, 5)
-        canvas.move(stem, 0, 5)
-        canvas.move(left_arm, 0, 5)
-        canvas.move(right_arm, 0, 5)
-        canvas.move(left_leg, 0, 5)
-        canvas.move(right_leg, 0, 5)
-        canvas.move(face, 0, 5)
-        canvas.move(left_eye, 0, 5)
-        canvas.move(right_eye, 0, 5)
-        canvas.move(mouth, 0, 5)
-        canvas.move(message, 0, 5)
-        root.update()
-        time.sleep(0.05)
+    if ctypes.windll.user32.GetAsyncKeyState(ord('P')):
+        break
 
-root.mainloop()
+    minimize_window()
+    ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), SW_MINIMIZE) # Minimize console window
--- 02_Rounds_GUI_V2.py
+++ 02_Rounds_GUI_V2.py
@@ -19,29 +19,40 @@ class Rounds:
                                     )
         self.rounds_heading.grid(row=0)
 
-        instructions = "Instructions go here..."
+        instructions = "Fear or Fail is a super spectacular quiz. You will be given " \
+                       "4 options to choose from for each question, simply click the " \
+                       "answer you want to choose, and you will be told whether you are " \
+                       "right or wrong, and which answer the correct one is." \
+                       "\n\n" \
+                       "In order to start, simply enter the number of rounds you want to play " \
+                       "between 1 and 113 rounds in the field below, and press enter or start to begin the quiz." \
+                       "\n\n" \
+                       "*Please note that Fear or Fail is not responsible for any harm that comes to the " \
+                       "participant in the event they do not get a perfect score" \
+                       "" \
+                       "\n\n\n" \
+                       "** TLDR: ENTER ROUNDS BETWEEN 1 AND 113 AND PRESS START, THEN CLICK THE CORRECT ANSWER"
 
         self.rounds_instructions = Label(self.rounds_frame,
                                          text=instructions,
-                                         wraplength=400, width=60,
-                                         justify="center")
+                                         wraplength=300, width=60,
+                                         justify="left")
         self.rounds_instructions.grid(row=1, pady=20)
 
-        rounds_error = "Please enter the number of rounds rou want to play between 1-113"
-
-        self.rounds_error = Label(self.rounds_frame, text="ERROR GOES HERE",
-                                  fg="#9C0000", font=("bold"))
+        self.rounds_error = Label(self.rounds_frame, text="",
+                                  fg="#9C0000", font="bold")
         self.rounds_error.grid(row=3)
 
         self.start_frame = Frame(padx=10, pady=10)
         self.start_frame.grid()
 
         self.entry_rounds_frame = Entry(self.start_frame,
-                                        width=12, font=("Arial", "16"))
+                                        width=9, font=("Arial", "19"))
         self.entry_rounds_frame.grid(row=4, column=0, pady=5)
 
-        self.start_button = Button(self.start_frame, width=6, text="Start",
-                                   font=("Arial", "12", "bold",), bg="#429E9D")
+        self.start_button = Button(self.start_frame, width=12, text="Start",
+                                   font=("Arial", "12", "bold",), bg="#429E9D",
+                                   command= self.rounds_check)
         self.start_button.grid(row=4, column=1, pady=5)
 
         # Conversion, help and statistics
@@ -53,8 +64,27 @@ class Rounds:
         self.help_button.grid(row=5, column=0, padx=10, pady=10)
 
         self.statistics_button = Button(self.start_frame, width=12, text="Statistics",
-                                  bg="#CCE5FF", font=("Arial", "12", "bold"))
+                                        bg="#CCE5FF", font=("Arial", "12", "bold"))
         self.statistics_button.grid(row=5, column=1, padx=10, pady=10)
+
+    def check_rounds(self, min_value, max_value):
+        error = "Please enter the number of rounds rounds \n you want to play between 1 - 113"
+
+        try:
+            response = self.entry_rounds_frame.get()
+            response = int(response)
+
+            if response < min_value or response > max_value:
+                self.rounds_error.config(text=error, fg="#9C0000")
+            else:
+                self.rounds_error.config(text="You chose to play {} rounds".format(response), fg="#32CD32")
+                return response
+
+        except ValueError:
+            self.rounds_error.config(text=error,fg="#9C0000")
+
+    def rounds_check(self):
+        self.check_rounds(1, 113)
 
 
 # main routine
